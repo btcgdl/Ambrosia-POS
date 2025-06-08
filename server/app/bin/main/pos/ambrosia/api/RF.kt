e88e88cb9b4e2d6a131f7c23023a5ea49355a61c
Vidarte-Alberto <a19100306@ceti.mx>
2025-06-08 02:14:46 -0600

    (feat): add jwt to login and solve app.kt

---
+++ server/app/bin/main/pos/ambrosia/api/RF.kt
@@ -0,0 +1,10 @@
+package pos.ambrosia.api
+
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.auth.*
+import io.ktor.server.routing.*
+
+fun Route.rfApi(
+) {
+    
+}
---
+++ server/app/bin/main/pos/ambrosia/util/Exceptions.kt
@@ -0,0 +1,5 @@
+package pos.ambrosia.utils
+
+// Custom exceptions for specific error scenarios
+class InvalidCredentialsException(message: String = "Invalid credentials") : IllegalArgumentException(message)
+class UnauthorizedApiException(message: String = "Unauthorized API access") : SecurityException(message)
