e5b976e93c53a1991cc33673329754d17033a858
JordyPirata <jordi77alva@gmail.com>
2025-06-09 14:00:07 -0600

    (refactor): move exception handling to separate Handler module

--- server/app/src/main/kotlin/pos/ambrosia/App.kt
+++ server/app/src/main/kotlin/pos/ambrosia/App.kt
@@ -13,12 +13,9 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
-import java.sql.SQLException
 import org.slf4j.LoggerFactory
 import pos.ambrosia.api.*
 import pos.ambrosia.config.AppConfig
-import pos.ambrosia.models.Message
-import pos.ambrosia.utils.InvalidCredentialsException
 import pos.ambrosia.utils.UnauthorizedApiException
 
 private val logger = LoggerFactory.getLogger("pos.ambrosia.App")
@@ -30,40 +27,8 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-    // Exception handling
-    install(StatusPages) {
-        exception<SQLException> { call, cause ->
-            logger.error("Database connection error: ${cause.message}", cause)
-            call.respond(
-                    HttpStatusCode.InternalServerError,
-                    Message("Error connecting to the database")
-            )
-        }
-        exception<Throwable> { call, cause ->
-            logger.error("Unhandled Throwable: ${cause.message}", cause)
-            call.respondText(
-                    text = cause.message ?: "",
-                    status = defaultExceptionStatusCode(cause) ?: HttpStatusCode.InternalServerError
-            )
-        }
-        exception<InvalidCredentialsException> { call, cause ->
-            logger.warn("Invalid login attempt: ${cause.message}")
-            call.respond(HttpStatusCode.Unauthorized, Message("Invalid credentials"))
-        }
-        exception<Exception> { call, cause ->
-            logger.error("Unhandled exception: ${cause.message}", cause)
-            call.respond(HttpStatusCode.InternalServerError, Message("Internal server error"))
-        }
-        exception<UnauthorizedApiException> { call, _ ->
-            logger.warn("Unauthorized API access attempt")
-            call.respond(HttpStatusCode.Forbidden, Message("Unauthorized API access"))
-        }
-        status(HttpStatusCode.NotFound) { call, status ->
-            logger.info("Resource not found: ${call.request}")
-            call.respondText(text = "Unknown endpoint (check api doc)", status = status)
-        }
-    }
     // Configure the application
+    Handler()// Install exception handlers
     install(ContentNegotiation) { json() }
     install(CORS) {
         anyHost()
---
+++ server/app/src/main/kotlin/pos/ambrosia/api/Handler.kt
@@ -0,0 +1,56 @@
+package pos.ambrosia.api
+
+import io.ktor.http.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import pos.ambrosia.utils.InvalidCredentialsException
+import pos.ambrosia.utils.UnauthorizedApiException
+import io.ktor.server.application.*
+import io.ktor.server.engine.defaultExceptionStatusCode
+
+import org.slf4j.LoggerFactory
+import java.sql.SQLException
+import pos.ambrosia.models.Message
+import pos.ambrosia.config.AppConfig
+
+
+private val logger = LoggerFactory.getLogger("pos.ambrosia.Handler")
+
+fun Application.Handler() {
+    install(StatusPages) {
+        exception<SQLException> { call, cause ->
+            logger.error("Database connection error: ${cause.message}", cause)
+            call.respond(
+                    HttpStatusCode.InternalServerError,
+                    Message("Error connecting to the database")
+            )
+        }
+        exception<Throwable> { call, cause ->
+            logger.error("Unhandled Throwable: ${cause.message}", cause)
+            call.respondText(
+                    text = cause.message ?: "",
+                    status = defaultExceptionStatusCode(cause) ?: HttpStatusCode.InternalServerError
+            )
+        }
+        exception<InvalidCredentialsException> { call, cause ->
+            logger.warn("Invalid login attempt: ${cause.message}")
+            call.respond(HttpStatusCode.Unauthorized, Message("Invalid credentials"))
+        }
+        exception<Exception> { call, cause ->
+            logger.error("Unhandled exception: ${cause.message}")
+            call.respond(HttpStatusCode.InternalServerError, Message("Internal server error"))
+        }
+        exception<UnauthorizedApiException> { call, _ ->
+            logger.warn("Unauthorized API access attempt")
+            call.respond(HttpStatusCode.Forbidden, Message("Unauthorized API access"))
+        }
+        status(HttpStatusCode.NotFound) { call, status ->
+            logger.info("Resource not found: ${call.request}")
+            call.respondText(text = "Unknown endpoint (check api doc)", status = status)
+        }
+        status(HttpStatusCode.BadRequest) { call, status ->
+            logger.warn("Bad request: ${call.request}")
+            call.respondText(text = "Bad request (check api doc)", status = status)
+        }
+    }
+}
