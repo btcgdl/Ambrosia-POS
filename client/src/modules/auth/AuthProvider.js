2772554504f694e8bb46e5dec8266d3d8800cffa
angel <bolo402402@gmail.com>
2025-07-09 15:23:07 -0600

    allow-credentials-server

--- client/src/modules/auth/AuthProvider.js
+++ client/src/modules/auth/AuthProvider.js
@@ -10,6 +10,7 @@ export function AuthProvider({ children }) {
         try {
             const response = await RefreshToken();
             console.log(response);
+            console.log(document.cookie);
         } catch (err) {
             console.error('Error en la petición de refresh:', err);
             logout();
