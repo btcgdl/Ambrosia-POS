6596184b9bc1852923b3c6912fe6fada0a80b9bb
themikemoniker <themikemoniker@proton.me>
2025-08-05 17:17:06 -0600

    add default user if there are no users

---
+++ server/app/src/main/kotlin/pos/ambrosia/services/DefaultCredentials.kt
@@ -0,0 +1,41 @@
+package pos.ambrosia.services
+
+import java.sql.Connection
+import pos.ambrosia.models.Role
+import pos.ambrosia.models.User
+
+class DefaultCredentialsService(private val connection: Connection) {
+
+  suspend fun addUser(): String {
+
+    val roleService = RolesService(connection)
+    val userService = UsersService(connection)
+
+    val userCount = userService.getUserCount()
+
+    // Only create default user if no users exist
+    if (userCount == 0L) {
+      val newRoleId = roleService.addRole(Role(null, "coolrolename", "password123", true  ))
+      if (newRoleId == null) {
+        throw Error()
+      }
+      val userCreationStatus = userService.addUser(User(null, "cooluser1", "0000", null, newRoleId ))
+      if (userCreationStatus == null) {
+        throw Error()
+      }
+      return userCreationStatus
+    }
+    return "Users already exist"
+  }
+
+}
+
+/*
+data class User(
+        val id: String? = null,
+        val name: String,
+        val pin: String,
+        val refreshToken: String? = null,
+        val role: String? = null
+)
+*/
