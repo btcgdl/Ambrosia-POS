c1fa5a9c2b2debe5f48e437f08b4fb8e7637d6dd
JordyPirata <jordi77alva@gmail.com>
2025-09-13 21:05:24 -0600

    feat: add printer configuration to the application module

--- server/app/src/main/kotlin/pos/ambrosia/api/Printer.kt
+++ server/app/src/main/kotlin/pos/ambrosia/api/Printer.kt
@@ -1,25 +1,31 @@
 package pos.ambrosia.api
+
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import io.ktor.server.response.*
 import com.github.anastaciocintra.escpos.EscPos
 import com.github.anastaciocintra.output.PrinterOutputStream
 import io.ktor.http.*
 
-fun Route.configurePrinter() {
-    route("/printers") {
-        get {
-            val printServices = PrinterOutputStream.getListPrintServicesNames().toList()
-            // call.respond(HttpStatusCode.OK, printServices)
-        }
-        post("/set") {
-            // TODO: Implement printer settings endpoint
-        }
-        post("/print") {
-            val printService = PrinterOutputStream.getPrintServiceByName("Your_Printer_Name_Here")
-            val printerOutputStream = PrinterOutputStream(printService)
-            val escpos = EscPos(printerOutputStream)
-            escpos.writeLF("Hello world")
-            escpos.feed(5).cut(EscPos.CutMode.FULL)
-            escpos.close()
+fun Application.configurePrinter() {
+    routing {
+        route("/printers") {
+            get {
+                // respond health check
+                call.respondText("Printer API is running", ContentType.Text.Plain, HttpStatusCode.OK)
+            }
+            post("/set") {
+                // TODO: Implement printer settings endpoint
+            }
+            post("/print") {
+                val printService = PrinterOutputStream.getPrintServiceByName("xt8769")
+                val printerOutputStream = PrinterOutputStream(printService)
+                val escpos = EscPos(printerOutputStream)
+                escpos.writeLF("Hello world")
+                escpos.feed(5).cut(EscPos.CutMode.FULL)
+                escpos.close()
+                call.respondText("Print job sent", ContentType.Text.Plain, HttpStatusCode.OK)
+            }
         }
     }
 }
