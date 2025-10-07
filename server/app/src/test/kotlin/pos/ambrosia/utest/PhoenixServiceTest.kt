7cf8ee6949a8ccd9a334538d3b8bf75af1dcf835
Carlos Ruz <carlosruzgarcia@gmail.com>
2025-10-07 12:52:15 -0600

    Add RoleService tests, WIP

--- server/app/src/test/kotlin/pos/ambrosia/utest/PhoenixServiceTest.kt
+++ server/app/src/test/kotlin/pos/ambrosia/utest/PhoenixServiceTest.kt
@@ -1,17 +1,24 @@
 // package pos.ambrosia.utest
 
 // import kotlinx.coroutines.runBlocking
+// import org.mockito.ArgumentMatchers.contains
 // import org.mockito.kotlin.*
-// import pos.ambrosia.models.AuthResponse
-// import pos.ambrosia.services.AuthService
+// import pos.ambrosia.services.PhoenixService
 // import java.sql.Connection
 // import java.sql.PreparedStatement
 // import java.sql.ResultSet
 // import kotlin.test.*
 
-// class AuthServiceTest {
+// class PhoenixServiceTest {
 //     private val mockConnection: Connection = mock()
 //     private val mockStatement: PreparedStatement = mock()
 //     private val mockResultSet: ResultSet = mock()
-    
+
+//     @Test
+//     fun`getNodeInfo returns valid NodeInfo JSON`() {
+//         runBlocking {
+
+//         }
+//     }
 // }
+
