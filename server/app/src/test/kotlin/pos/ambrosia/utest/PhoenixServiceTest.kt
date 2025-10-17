1b372f3db156a3808163937c0e8395d4459ba3e4
Carlos Ruz <carlosruzgarcia@gmail.com>
2025-10-17 15:58:34 -0600

    WIP: Add first test for Phoenix Service

--- server/app/src/test/kotlin/pos/ambrosia/utest/PhoenixServiceTest.kt
+++ server/app/src/test/kotlin/pos/ambrosia/utest/PhoenixServiceTest.kt
@@ -1,24 +1,58 @@
-// package pos.ambrosia.utest
+package pos.ambrosia.utest
 
-// import kotlinx.coroutines.runBlocking
-// import org.mockito.ArgumentMatchers.contains
-// import org.mockito.kotlin.*
-// import pos.ambrosia.services.PhoenixService
-// import java.sql.Connection
-// import java.sql.PreparedStatement
-// import java.sql.ResultSet
-// import kotlin.test.*
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.mock.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import pos.ambrosia.models.Phoenix.NodeInfo
+import kotlin.test.*
 
-// class PhoenixServiceTest {
-//     private val mockConnection: Connection = mock()
-//     private val mockStatement: PreparedStatement = mock()
-//     private val mockResultSet: ResultSet = mock()
+class PhoenixServiceTest {
 
-//     @Test
-//     fun`getNodeInfo returns valid NodeInfo JSON`() {
-//         runBlocking {
+    @Test
+    fun `getNodeInfo returns NodeInfo on success`() {
+        runBlocking {
+            // Arrange
+            val mockJson = """
+                {
+                    "nodeId": "02f3c93f2bsd...",
+                    "channels": [],
+                    "chain": "mainnet",
+                    "blockHeight": 800000,
+                    "version": "v0.1.0"
+                }
+            """.trimIndent()
 
-//         }
-//     }
-// }
+            val mockEngine = MockEngine { request ->
+                respond(
+                    content = ByteReadChannel(mockJson.toByteArray(Charsets.UTF_8)),
+                    status = HttpStatusCode.OK,
+                    headers = headersOf(HttpHeaders.ContentType, "application/json")
+                )
+            }
 
+            val httpClient = HttpClient(mockEngine) {
+                install(ContentNegotiation) {
+                    json(Json {
+                        ignoreUnknownKeys = true
+                    })
+                }
+            }
+
+            // Act
+            val response = httpClient.get("/getinfo")
+            val nodeInfo = response.body<NodeInfo>()
+
+            // Assert
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("mainnet", nodeInfo.chain)
+            assertEquals(800000, nodeInfo.blockHeight)
+        }
+    }
+}
