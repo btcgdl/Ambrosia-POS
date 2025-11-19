80153f888f858c123b75fe0ab982b651a8d24808
Ivan Robles <irae45@gmail.com>
2025-11-18 20:08:22 -0600

    add store users translations.

--- client/src/components/pages/Store/locales/en.js
+++ client/src/components/pages/Store/locales/en.js
@@ -14,7 +14,32 @@ const store_en = {
       products: "Products",
       sales: "Sales",
     },
-  }
+  },
+  users: {
+    title: "Users",
+    subtitle: "Manage your store staff",
+    addUser: "Add User",
+    name: "Name",
+    role: "Role",
+    email: "Email",
+    phone: "Phone",
+    status: "Status",
+    actions: "Actions",
+    modal: {
+      title: "Add User",
+      userNameLabel: "Name",
+      userNamePlaceholder: "Enter User Name",
+      userPinLabel: "PIN",
+      userPinPlaceholder: "Enter a 4-digit PIN",
+      userPhoneLabel: "Phone",
+      userPhonePlaceholder: "Ej: 3312345678",
+      userEmailLabel: "Email",
+      userEmailPlaceholder: "contact@awesomebusiness.com",
+      userRoleLabel: "Role",
+      submitButton: "Add",
+      cancelButton: "Cancel",
+    }
+  },
 };
 
 export default store_en;
